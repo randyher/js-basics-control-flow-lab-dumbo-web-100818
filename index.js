46f63ff8eac09224f5c6c26f737b8aacf62ee442
Randy Herasme <flatironschool@Flatirons-MacBook-Air.local>
2018-11-18 09:56:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code in this file!
+function scuberGreetingForFeet(feet){
+  if (feet > 2500){
+    return `No can do.`
+  } if (feet > 2000){
+    return 'I will gladly take your thirty bucks.'
+  } if (feet <= 400){
+    return `This one is on me!`
+  }
+}
+
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return "Thank you so much.";
+      break ;
+    case 'not as generous':
+    return "Thank you.";
+    break;
+    default:
+      return "Bye."
+  }
+}
